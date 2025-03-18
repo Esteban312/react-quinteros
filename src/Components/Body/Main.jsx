a0d1e9eb09b241e002154870bba50e41058e8486
Esteban Quinteros <esteban.quinteros993@gmail.com>
2025-03-18 18:01:33 -0300

    reacomodande los productos dentro de ItemListContainer

--- src/Components/Body/Main.jsx
+++ src/Components/Body/Main.jsx
@@ -1,20 +1,13 @@
 import './Main.css'
 import ItemListContainer from '../ItemListContainer/ItemListContainer'
-import Producto from '../Product/Producto'
+
 
 function Main() {
 
     return (
         <>
         <main>
-            <section>
                 <ItemListContainer greetings={"Bienvenidos a PianoCenter"}/>
-            </section>
-            <section className='product-container'>
-                <Producto imgProducto={'/product-piano.jpg'} productTitle='Piano Vertical Marrón' precio='$200000'/>
-                <Producto imgProducto={'/product-drum.jpg'} productTitle='Bateria completa' precio='$350000'/>
-                <Producto imgProducto={'/producto-guitar.jpg'} productTitle='Guitarra electrica' precio='$100000'/>
-            </section>
         </main>
         </>
     )
--- src/Components/ItemListContainer/ItemListContainer.jsx
+++ src/Components/ItemListContainer/ItemListContainer.jsx
@@ -1,8 +1,17 @@
+import Producto from '../Product/Producto'
+
 function ItemListContainer({greetings}) {
 
     return (
         <>
-            <h1>{greetings}</h1>
+            <section>
+                <h1>{greetings}</h1>
+            </section>
+            <section className='product-container'>
+                <Producto imgProducto={'/product-piano.jpg'} productTitle='Piano Vertical Marrón' precio='$200000'/>
+                <Producto imgProducto={'/product-drum.jpg'} productTitle='Bateria completa' precio='$350000'/>
+                <Producto imgProducto={'/producto-guitar.jpg'} productTitle='Guitarra electrica' precio='$100000'/>
+            </section>
         </>
     )
     }
